bdd10490ec0f583d4a326a2e306ea0e850f67761
AlexeyPavlov <pavlov@game-insight.com>
2022-08-12 13:38:11 +0300

    Upd: breadth search

---
+++ breadth_search/Program.cs
@@ -0,0 +1,26 @@
+﻿
+var graph = new Dictionary<string, string[]> {
+   { "you", new string[] { "jane", "maggie" } },
+   { "jane", new string[] { "bob", "alen" } }
+};
+
+var queue = new Queue<string>();
+
+foreach (var member in graph["you"])
+    queue.Enqueue(member);
+
+
+while (queue.Count > 0)
+{
+    var member = queue.Dequeue();
+    if (member == "bob")
+    {
+        Console.WriteLine($"Found seller! {member} is seller");
+    }
+    else if (graph.TryGetValue(member, out var members))
+    {
+
+        foreach (var m in members)
+            queue.Enqueue(m);
+    }
+}
