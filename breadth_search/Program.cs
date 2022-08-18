688ae49dea8bffb85730070ba8e0f284074ab68b
AlexeyPavlov <pavlov@game-insight.com>
2022-08-18 13:13:19 +0300

    New: dijkstra

--- breadth_search/Program.cs
+++ breadth_search/Program.cs
@@ -1,28 +1,43 @@
 ﻿
-var graph = new Dictionary<string, string[]> {
-   { "you", new string[] { "jane", "maggie" } },
-   { "jane", new string[] { "bob", "alen" } }
-};
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-var queue = new Queue<string>();
-var searched = new Dictionary<string, int>();
-
-foreach (var member in graph["you"])
-    queue.Enqueue(member);
-
-
-while (queue.Count > 0)
+namespace breadth_search
 {
-    var member = queue.Dequeue();
-    if (member == "bob")
+    class Program
     {
-        Console.WriteLine($"Found seller! {member} is seller");
-    }
-    else if (!searched.ContainsKey(member) && graph.TryGetValue(member, out var members))
-    {
-        foreach (var m in members)
-            queue.Enqueue(m);
+        static void Main(string[] args)
+        {
+            var graph = new Dictionary<string, string[]> {
+                { "you", new string[] { "jane", "maggie" } },
+                { "jane", new string[] { "bob", "alen" } }
+            };
 
-        searched[member] = 1;
+            var queue = new Queue<string>();
+            var searched = new Dictionary<string, int>();
+
+            foreach (var member in graph["you"])
+                queue.Enqueue(member);
+
+
+            while (queue.Count > 0)
+            {
+                var member = queue.Dequeue();
+                if (member == "bob")
+                {
+                    Console.WriteLine($"Found seller! {member} is seller");
+                }
+                else if (!searched.ContainsKey(member) && graph.TryGetValue(member, out var members))
+                {
+                    foreach (var m in members)
+                        queue.Enqueue(m);
+
+                    searched[member] = 1;
+                }
+            }
+        }
     }
 }
---
+++ dijkstra/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dijkstra
+{
+    class Program
+    {
+
+        static Dictionary<string, int> Processed = new Dictionary<string, int>();
+
+        static void Main(string[] args)
+        {
+            var graph = new Dictionary<string, Dictionary<string, int>>
+            {
+                ["start"] = new Dictionary<string, int>
+                {
+                    ["a"] = 6,
+                    ["b"] = 2
+                },
+                ["a"] = new Dictionary<string, int>
+                {
+                    ["fin"] = 1
+                },
+                ["b"] = new Dictionary<string, int>
+                {
+                    ["a"] = 3,
+                    ["fin"] = 5
+                },
+                ["fin"] = new Dictionary<string, int>()
+            };
+
+            var costs = new Dictionary<string, int>
+            {
+                ["a"] = 6,
+                ["b"] = 2,
+                ["fin"] = int.MaxValue
+            };
+
+            var parents = new Dictionary<string, string>
+            {
+                ["a"] = "start",
+                ["b"] = "start",
+                ["fin"] = null
+            };
+
+            var node = FindLowestCostNode(costs);
+            while (!String.IsNullOrEmpty(node)) {
+
+                var cost = costs[node];
+                var neighbors = graph[node];
+
+                foreach(var i in neighbors) {
+
+                    var newCost = cost + i.Value;
+                    if (costs[i.Key] > newCost) {
+                        costs[i.Key] = newCost;
+                        parents[i.Key] = node;
+                    }
+                }
+
+                Processed[node] = 1;
+                node = FindLowestCostNode(costs);
+            }
+
+            foreach ( var i in costs ) {
+                Console.WriteLine($"{i.Key} : {i.Value}");
+            }
+
+            foreach ( var i in parents ) {
+                Console.WriteLine($"{i.Key} : {i.Value}");
+            }
+        }
+
+        public static string FindLowestCostNode(Dictionary<string, int> costs) 
+        {
+            var lowestCostNode = String.Empty;
+            var lowestCost = int.MaxValue;
+
+            foreach( var i in costs ) {
+
+                if (i.Value < lowestCost && !Processed.ContainsKey(i.Key))
+                {
+                    lowestCost = i.Value;
+                    lowestCostNode = i.Key;
+                }
+            }
+
+            return lowestCostNode;
+        }
+    }
+}
