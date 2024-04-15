ba275ee5cfc7c26220a251e9f2aa4dc7a80a0d0d
Aleksey Pavlov <pawlovv@gmail.com>
2024-04-16 00:40:20 +0500

    New: find_in_matrix. merge_arrays.

---
+++ find_in_matrix/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+
+Find target in matrix:
+
+Input:
+[
+    [1,3,5,7],
+    [10,11,16,20],
+    [23,30,34,60]
+];
+
+target = 3;
+
+Output: true
+
+*/
+
+// wrost O(n log n)
+// best O(log n)
+
+int[][] matrix = [
+    [1,3,5,7],
+    [10,11,16,20],
+    [23,30,34,60]
+];
+
+int target = 34;
+
+
+for (var i = 0; i < matrix.Length; i++)
+{
+    if (matrix[i][matrix[i].Length - 1] > target)
+    {
+        var arr = matrix[i];
+        var start = 0;
+        var end = matrix[i].Length;
+
+        while (start <= end)
+        {
+            var mid = (start + end) / 2;
+
+            if (arr[mid] == target)
+            {
+                Console.WriteLine(true);
+                break;
+            }
+            else if (arr[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else if (arr[mid] < target)
+            {
+                start = mid + 1;
+            }
+        }
+    }
+}
---
+++ merge_arrays/Program.cs
@@ -0,0 +1,41 @@
+﻿// input [1,2,3]  [1,3,4]
+// output [1,1,2,3,3,4]
+// O(n)
+
+int[] arr1 = [1, 2, 3, 5, 25];
+int[] arr2 = [1, 3, 4, 10, 20];
+
+List<int> result = [];
+
+
+for (int i = 0; i < Math.Max(arr1.Length, arr2.Length); i++)
+{
+    if (i > arr1.Length - 1)
+    {
+        result.Add(arr2[i]);
+        continue;
+    }
+    else if (i > arr2.Length - 1)
+    {
+        result.Add(arr1[i]);
+        continue;
+    }
+
+    if (arr1[i] >= arr2[i])
+    {
+        result.Add(arr2[i]);
+        result.Add(arr1[i]);
+
+    }
+    else
+    {
+        result.Add(arr1[i]);
+        result.Add(arr2[i]);
+    }
+}
+
+
+foreach (int i in result)
+{
+    Console.WriteLine(i);
+}
