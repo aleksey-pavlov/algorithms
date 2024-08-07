4dc507537bbbb42c28b1d87535211fba6ba42c3d
Aleksey Pavlov <pawlovv@gmail.com>
2024-08-07 14:15:28 +0300

    Upd: twosum and addtwonumbers

--- addtwonumbers/Program.cs
+++ addtwonumbers/Program.cs
@@ -1,17 +1,57 @@
 ﻿/* https://leetcode.com/problems/add-two-numbers/ */
 
-using System.Diagnostics;
-using Addtwonumbers;
+using System.Numerics;
 
-
-var sol = new Solution();
 
 var list1 = new ListNode(2, new ListNode(4, new ListNode(3)));
 var list2 = new ListNode(5, new ListNode(6, new ListNode(4)));
 
-var res = sol.AddTwoNumbers(list1, list2);
+var res = AddTwoNumbers(list1, list2);
 
 while (res != null) {
     Console.WriteLine(res.val);
     res = res.next;
+}
+
+
+ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+{
+    var num1 = "";
+    var num2 = "";
+
+    while (l1 != null || l2 != null)
+    {
+        if (l1 != null)
+        {
+            num1 = l1.val + num1;
+            l1 = l1.next;
+        }
+
+        if (l2 != null)
+        {
+            num2 = l2.val + num2;
+            l2 = l2.next;
+        }
+    }
+
+    var res = BigInteger.Parse(num1) + BigInteger.Parse(num2);
+
+    ListNode list = null;
+    foreach (var i in res.ToString())
+    {
+        list = new ListNode(int.Parse(i.ToString()), list);
+    }
+
+    return list;
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode? next;
+    public ListNode(int val = 0, ListNode? next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
 }
--- twosum/Program.cs
+++ twosum/Program.cs
@@ -1,13 +1,35 @@
 ﻿/* https://leetcode.com/problems/two-sum/ */
-using TwoSum;
-
-var app = new Solution();
-
-var res1 = app.TwoSum(new int[] { 1, 2, 3, 7, 11, 15 }, 9);
+var res1 = TwoSum(new int[] { 1, 2, 3, 7, 11, 15 }, 9);
 
 Console.WriteLine(string.Join(", ", res1));
 
 
-var res2 = app.TwoSum(new int[] { 3, 2, 4 }, 6);
+var res2 = TwoSum(new int[] { 3, 2, 4 }, 6);
 
 Console.WriteLine(string.Join(", ", res2));
+
+
+int[] TwoSum(int[] nums, int target)
+{
+    if (nums.Length == 2)
+        return new int[] { 0, 1 };
+
+    var map = new Dictionary<int, int>();
+
+    for (var i = 0; i < nums.Length; i++)
+    {
+        map[nums[i]] = i;
+    }
+
+    for (var i = 0; i < nums.Length; i++)
+    {
+        var remind = target - nums[i];
+
+        if (map.TryGetValue(remind, out int left) && left != i)
+        {
+            return new int[2] { i, left };
+        }
+    }
+
+    return new int[0];
+}
